650568c7a1aaf529ba4e96178cbd30b77ea277b9
Martin Zikmund <martin@zikmund.dev>
2023-02-11 13:27:27 +0100

    Add support for learn.microsoft.com

--- src/background.js
+++ src/background.js
@@ -1,9 +1,9 @@
-const regex = /(.*docs.microsoft.com\/)([a-z][a-z]\-[a-z][a-z]]*)(\/.*)/g;
+const regex = /(.*(learn\.microsoft\.com|docs\.microsoft\.com)\/)([a-z][a-z]-[a-z][a-z])(\/.*)/g;
 
-function getFirstGroup(regexp, str) {
+function getLocalizationGroup(regexp, str) {
     return Array.from(str.matchAll(regexp), m => {
-      if (m.length > 2){
-        return m[2]
+      if (m.length > 3){
+        return m[3]
       } else{
         return [];
       };
@@ -12,10 +12,10 @@ function getFirstGroup(regexp, str) {
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        var group = getFirstGroup(regex, details.url);        
+        var group = getLocalizationGroup(regex, details.url);        
         if (group.length > 0 && group[0].toLowerCase() != "en-us"){
-            var newUrl = details.url.replace(regex, "$1en-us$3");
+            var newUrl = details.url.replace(regex, "$1en-us$4");
             return { redirectUrl: newUrl }
         } 
-      }, { urls: ['*://docs.microsoft.com/*'] }, ['blocking']
+      }, { urls: ['*://docs.microsoft.com/*', '*://learn.microsoft.com/*'] }, ['blocking']
 );
